6e400c74db6021548a05e6e7dcc16b0a180d86ef
Alexey Kovalev <41exey.kov41ev@gmail.com>
2021-03-06 22:40:15 +0300

    отформатирован код, добавлено ru отображение дат, добавлено альтернативное решение задания #20
    - Отформатирован код для повышения читаемости.
    - Добавлена строка, позволяющая изменить локаль на RU для текущей сессии. Таким образом изменено отображение дат прописью.
    - Добавлено лаконичное альтернативное решение для задачи #20.
    ---------
    Добавлены небольшие оптимизации и исправления в код.

--- Тема_02_Ковалев_Алексей_контрольная_6_марта.sql
+++ Тема_02_Ковалев_Алексей_контрольная_6_марта.sql
@@ -1,34 +1,46 @@
+set lc_time_names = 'ru_RU';  -- отображение даты на русском языке
+
 -- 1
-select concat_ws(';', STUDENT_ID, ucase(SURNAME), ucase(NAME), STIPEND,
-    KURS, ucase(CITY), date_format(BIRTHDAY, '%d/%m/%Y'), UNIV_ID)
-    as "RESULT" from student;
+select concat_ws(
+    ';',
+    STUDENT_ID,
+    ucase(SURNAME),
+    ucase(NAME),
+    STIPEND,
+    KURS,
+    ucase(CITY),
+    date_format(BIRTHDAY, '%d/%m/%Y'),
+    UNIV_ID
+) as "RESULT" from student;
 
 -- 2
-select concat(concat_ws(
-    '; ', concat(substr(ucase(NAME), 1, 1), '.', ucase(SURNAME)),
-    concat('место жительства-', ucase(CITY)), 'родился-'
-    ),
-    date_format(BIRTHDAY, '%d.%m.%y')) as "RESULT" from student;
+select concat_ws(
+    '; ',
+    concat(substr(ucase(NAME), 1, 1), '.', ucase(SURNAME)),
+    concat('место жительства-', ucase(CITY)),
+    concat('родился - ', date_format(BIRTHDAY, '%d.%m.%y'), '.')
+) as "RESULT" from student;
 
 -- 3
-select concat(concat_ws(
-    '; ', concat(substr(lcase(NAME), 1, 1), '.', lcase(SURNAME)),
-    concat('место жительства-', lcase(CITY)), 'родился: '
-    ),
-    lcase(date_format(BIRTHDAY, '%d-%m-%y')), '.') as "RESULT" from student;
+select lcase(concat_ws(
+    '; ',
+    concat(substr(NAME, 1, 1), '.', SURNAME),
+    concat('место жительства-', CITY),
+    concat('родился: ', date_format(BIRTHDAY, '%d-%b-%Y'), '.')
+)) as "RESULT" from student;
 
 -- 4
 select concat(
-    concat(ucase(left(NAME, 1)), substr(lcase(NAME), 2)),
+    ucase(left(NAME, 1)), substr(lcase(NAME), 2),
     ' ',
-    concat(ucase(left(SURNAME, 1)), substr(lcase(SURNAME), 2)),
+    ucase(left(SURNAME, 1)), substr(lcase(SURNAME), 2),
     ' родился в ',
     year(BIRTHDAY),
     ' году.'
 ) as "RESULT" from student;
 
 -- 5
-select SURNAME, NAME, STIPEND * 100 as "STIPEND" from student;
+select SURNAME, NAME, STIPEND * 100 as "DREAM_STIPEND" from student;
 
 -- 6
 select concat(
@@ -41,31 +53,19 @@ select concat(
 ) as "RESULT" from student where KURS in (1, 2, 4);
 
 -- 7
-select concat(
-    'Код-',
-    UNIV_ID,
+select concat_ws(
     '; ',
-    ucase(UNIV_NAME),
-    '-г.',
-    ucase(CITY),
-    '; ',
-    'Рейтиг=',
-    RATING,
-    '.'
+    concat('Код-', UNIV_ID),
+    concat(ucase(UNIV_NAME), '-г.', ucase(CITY)),
+    concat('Рейтиг=', RATING, '.')
 ) as "RESULT" from university;
 
 -- 8
-select concat(
-    'Код-',
-    UNIV_ID,
+select concat_ws(
     '; ',
-    ucase(UNIV_NAME),
-    '-г.',
-    ucase(CITY),
-    '; ',
-    'Рейтиг=',
-    round(RATING, -length(RATING) + 1),
-    '.'
+    concat('Код-', UNIV_ID),
+    concat(ucase(UNIV_NAME), '-г.', ucase(CITY)),
+    concat('Рейтиг=', round(RATING, -length(RATING) + 1), '.')
 ) as "RESULT" from university;
 
 -- 9
@@ -81,16 +81,16 @@ select STUDENT_ID, min(MARK) as "MIN_MARK" from exam_marks group by STUDENT_ID;
 select STUDENT_ID, max(MARK) as "MAX_MARK" from exam_marks group by STUDENT_ID;
 
 -- 13
-select * from student where lcase(SURNAME) like 'и%' order by SURNAME limit 1;
+select SURNAME from student where lcase(SURNAME) like 'и%' order by SURNAME limit 1;
 
 -- 14
 select SUBJ_NAME, max(SEMESTER) from subject group by SUBJ_ID;
 
 -- 15
-select count(STUDENT_ID) from exam_marks group by EXAM_DATE;
+select EXAM_DATE, count(distinct STUDENT_ID) from exam_marks group by EXAM_DATE;
 
 -- 16
-select KURS, SUBJ_ID, avg(MARK) from exam_marks natural join student group by KURS, SUBJ_ID;
+select KURS, SUBJ_ID, avg(MARK) from exam_marks natural join student group by KURS, SUBJ_ID order by KURS, SUBJ_ID;
 
 -- 17
 select STUDENT_ID, avg(MARK) from exam_marks group by STUDENT_ID;
@@ -99,7 +99,7 @@ select STUDENT_ID, avg(MARK) from exam_marks group by STUDENT_ID;
 select EXAM_ID, avg(MARK) from exam_marks group by EXAM_ID;
 
 -- 19
-select EXAM_ID, count(STUDENT_ID) from exam_marks where MARK > 2 group by EXAM_ID;
+select EXAM_ID, count(STUDENT_ID) from exam_marks group by EXAM_ID;
 
 -- 20
 with t as (
@@ -108,3 +108,6 @@ with t as (
             if(SEMESTER=7 or SEMESTER=8, 4, 0)))) as COURSE from subject
 )
 select COURSE, count(*) from t group by COURSE;
+
+-- 20 by https://github.com/GeorgiyDemo
+select (semester + 1) div 2 as "COURSE", count(*) from subject group by COURSE;
